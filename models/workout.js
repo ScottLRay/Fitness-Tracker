ca1d53e1aad2b3fc9ffaf31cf798a76a0d9c24c9
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-13 16:24:19 -0700

    trying to set up routes

--- models/workout.js
+++ models/workout.js
@@ -2,28 +2,39 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exercise = new Schema({
+const ExerciseSchema = new Schema({
   //name of exercise
   name: {
     type: String,
+    required: true,
   },
   //type of exercise
-  type: {},
+  type: {
+    type: String,
+    required: true,
+  },
   //how much weight
   weight: {
     type: Number,
+    required: true,
   },
   //the sets
   sets: {
     type: Number,
+    required: true,
   },
   //the reps
   reps: {
     type: Number,
+    required: true,
   },
   //duration of exercise
   duration: {
     type: Number,
+    required: true,
   },
 });
 
+const workout = mongoose.model("workout", ExerciseSchema)
+
+module.exports = workout
