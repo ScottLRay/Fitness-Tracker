1e497580948e638f9f70c14dcc2f0076f2658e40
unname666 <78314985+unname666@users.noreply.github.com>
2021-04-16 10:01:19 -0700

    trying to fix a bug

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static(__dirname + "/public"));
+// __dirname + 
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 app.use(require("./routes"));
+// app.use(require("./routes/api.js"));
+// app.use(require("./routes/views.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
